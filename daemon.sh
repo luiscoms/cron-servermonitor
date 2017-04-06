4165b400eb8410ff506dbdbc1219ed2a3a331302
Luis Fernando Gomes <dev@luiscoms.com.br>
2017-04-06 17:28:46 -0300

    Add sleeptime

--- daemon.sh
+++ daemon.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SLEEP_TIME=${SLEEP_TIME-60}
+
 sleep 10
 while [[ true ]]; do
-	echo "Running daemon" >> /var/log/cron.log
-	date >> /var/log/cron.log
+	echo "$(date) Running daemon every ${SLEEP_TIME} seconds" >> /var/log/cron.log
+	# date >> /var/log/cron.log
 	/usr/bin/php /var/www/html/cron/status.cron.php
-	echo "Sleeping daemon" >> /var/log/cron.log
+	echo "$(date) Sleeping daemon" >> /var/log/cron.log
 	sleep 60
 done
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 echo "Creating config file"
 ln -sf config/config.php config.php
-echo "Starting cron"
-service cron restart
+# echo "Starting cron"
+# service cron restart
 
 echo "Starting daemon"
 /usr/local/bin/daemon.sh &
