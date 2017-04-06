a85e08e8ece9a4a9596ba6a46a83ad958ecc3302
Luis Fernando Gomes <dev@luiscoms.com.br>
2017-04-06 17:33:43 -0300

    Add sleep seconds

--- daemon.sh
+++ daemon.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SLEEP_TIME=${SLEEP_TIME-60}
+SLEEP_SECONDS=${SLEEP_SECONDS-60}
 
 sleep 10
 while [[ true ]]; do
-	echo "$(date) Running daemon every ${SLEEP_TIME} seconds" >> /var/log/cron.log
+	echo "$(date) Running daemon every ${SLEEP_SECONDS} seconds" >> /var/log/cron.log
 	# date >> /var/log/cron.log
 	/usr/bin/php /var/www/html/cron/status.cron.php
 	echo "$(date) Sleeping daemon" >> /var/log/cron.log
-	sleep 60
+	sleep ${SLEEP_SECONDS}
 done
